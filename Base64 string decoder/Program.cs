8fb4cc240149699eb8c3a06c0a13657097645c4c
Xandor_Maker <Xandor.Maker@gmail.com>
2023-11-03 11:24:26 +0100

    Add project files.

---
+++ Base64 string decoder/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string inputFile = "Target.conf"; // Replace with the path to your input file
+        string outputFile = "result.txt"; // Replace with the desired output file
+
+        try
+        {
+            string[] base64Strings = File.ReadAllLines(inputFile);
+
+            using (StreamWriter writer = new StreamWriter(outputFile))
+            {
+                foreach (string base64Text in base64Strings)
+                {
+                    byte[] data = Convert.FromBase64String(base64Text);
+                    string decodedText = Encoding.UTF8.GetString(data);
+
+                    writer.WriteLine(decodedText);
+                }
+            }
+
+            Console.WriteLine("Decoded strings saved to " + outputFile);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+    }
+}
